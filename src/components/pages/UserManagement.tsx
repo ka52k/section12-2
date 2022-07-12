69571fb005e32e06621e4e8c0fb047dfc187b27c
ka52k <76809740+ka52k@users.noreply.github.com>
2022-07-12 19:26:02 +0900

    section12-81迄

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,18 +1,97 @@
-import { memo, VFC } from "react";
-import { Wrap, WrapItem } from "@chakra-ui/react";
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { memo, useCallback, useEffect, VFC } from "react";
+import {
+  Center,
+  // FormControl,
+  // FormLabel,
+  // Input,
+  // Modal,
+  // ModalBody,
+  // ModalCloseButton,
+  // ModalContent,
+  // ModalHeader,
+  // ModalOverlay,
+  Spinner,
+  // Stack,
+  useDisclosure,
+  Wrap,
+  WrapItem
+} from "@chakra-ui/react";
 
 import { UserCard } from "../organisms/user/UserCard";
+import { UserDetailModal } from "../organisms/user/UserDetailModal";
+import { useAllUsers } from "../../hooks/useAllUsers";
+import { useSelectUser } from "../../hooks/useSelectUser";
 
 export const UserManagement: VFC = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { getUsers, users, loading } = useAllUsers();
+  const { onSelectUser, selectedUser } = useSelectUser();
+  // console.log(selectedUser);
+
+  useEffect(() => getUsers(), []);
+
+  const onClickUser = useCallback((id: number) => {
+    // console.log(id);
+    onSelectUser({ id, users, onOpen });
+    // onOpen();
+  }, [users, onSelectUser, onOpen]);
+
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          imageUrl="https://source.unsplash.com/random"
-          userName="圭一"
-          fullName="Keiichi Kobayashi"
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} m="auto">
+              <UserCard
+                id={user.id}
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+                onClick={onClickUser}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+      <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose} />
+      {/* <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        autoFocus={false}
+        motionPreset="slideInBottom"
+      >
+        <ModalOverlay />
+        <ModalContent pb={6}>
+          <ModalHeader>ユーザー詳細</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody mx={4}>
+            <Stack spacing={4}>
+              <FormControl>
+                <FormLabel>名前</FormLabel>
+                <Input value="圭一" isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>フルネーム</FormLabel>
+                <Input value="Keiichi Kobayashi" isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>メール</FormLabel>
+                <Input value="kk@kk.com" isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>TEL</FormLabel>
+                <Input value="0000-123-4567" isReadOnly />
+              </FormControl>
+            </Stack>
+          </ModalBody>
+        </ModalContent>
+      </Modal> */}
+    </>
   );
 });
